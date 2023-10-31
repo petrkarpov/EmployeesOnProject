06f5c7867b425e08737fce14522241682115490a
PeterBurrfot <arr.in.the.pw@gmail.com>
2023-10-31 16:04:47 +0300

    EmployeesOnProject

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        final int PROGRAMMERS = 5;
+        final int TESTERS_FOR_ONE_PROGRAMMER = 1;
+        final int SUPPORT = 2;
+
+        final int NECESSARY_TESTERS = TESTERS_FOR_ONE_PROGRAMMER * PROGRAMMERS;
+
+        final int NECESSARY_SUPPORT = SUPPORT * PROGRAMMERS;
+
+        final int ALL_SPECIALISTS = PROGRAMMERS + NECESSARY_TESTERS + NECESSARY_SUPPORT;
+
+        System.out.println("Тестировщики, требуемые на проекте: " + NECESSARY_TESTERS);
+        System.out.println("Специалисты поддержки, требуемые на проекте: " + NECESSARY_SUPPORT);
+        System.out.println("Общее кол-во специалистов на проекте: " + ALL_SPECIALISTS);
+    }
+}
