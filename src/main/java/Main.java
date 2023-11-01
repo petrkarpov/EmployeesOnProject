a7d0df0b9ad5f79caf2333db523f5f0251332e2f
PeterBurrfot <arr.in.the.pw@gmail.com>
2023-11-01 18:22:59 +0300

    EmployeesOnProject

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        final int PROGRAMMERS = 5;
-        final int TESTERS_FOR_ONE_PROGRAMMER = 1;
-        final int SUPPORT = 2;
+        final int programmers = 5;
+        final int testersForOneProgrammer = 1;
+        final int support = 2;
+        final int necessaryTesters = testersForOneProgrammer * programmers;
+        final int necessarySupport = support * programmers;
+        final int allSpecialists = programmers + necessaryTesters + necessarySupport;
 
-        final int NECESSARY_TESTERS = TESTERS_FOR_ONE_PROGRAMMER * PROGRAMMERS;
-        final int NECESSARY_SUPPORT = SUPPORT * PROGRAMMERS;
-        final int ALL_SPECIALISTS = PROGRAMMERS + NECESSARY_TESTERS + NECESSARY_SUPPORT;
-
-        System.out.println("Тестировщики, требуемые на проекте: " + NECESSARY_TESTERS);
-        System.out.println("Специалисты поддержки, требуемые на проекте: " + NECESSARY_SUPPORT);
-        System.out.println("Общее кол-во специалистов на проекте: " + ALL_SPECIALISTS);
+        System.out.println("Тестировщики, требуемые на проекте: " + necessaryTesters);
+        System.out.println("Специалисты поддержки, требуемые на проекте: " + necessarySupport);
+        System.out.println("Общее кол-во специалистов на проекте: " + allSpecialists);
     }
 }
